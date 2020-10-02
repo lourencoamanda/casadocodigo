234169d1bb7b41e9a48d28a454037dfa9738a29d
Amanda Lourenco <amandalourenco67@gmail.com>
2020-10-02 13:40:06 -0300

    ajuste mensagem

--- src/main/java/br/com/casadocodigo/controller/AutorController.java
+++ src/main/java/br/com/casadocodigo/controller/AutorController.java
@@ -5,17 +5,13 @@ import br.com.casadocodigo.repositories.AutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 public class AutorController {
